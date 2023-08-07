4d882734939eb54a33c633d1df894b46631f8ff2
Kingsley Usa <kingsident@gmail.com>
2023-08-07 20:09:32 +0100

    Added assignment files and directory

---
+++ 0x00-python-hello_world/100-write.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+import sys as sis
+
+
+def main():
+
+    sentence = "and that piece of art is useful - Dora Korpar, 2015-10-19"
+    sis.stderr.write(sentence + "\n")
+    sis.exit
+
+
+if __name__ == "__main__":
+    main()
