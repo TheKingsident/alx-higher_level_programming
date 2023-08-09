b4b252391121a62d24e1b7fcb6a460a829a391fc
Kingsley Usa <kingsident@gmail.com>
2023-08-09 12:16:26 +0100

    Added assignment files and directory

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def fizzbuzz():
+    for n in range(1, 101):
+        if (n % 3 == 0) and (n % 5 != 0):
+            print("Fizz", end=" ")
+        elif (n % 5 == 0) and (n % 3 != 0):
+            if n == 100:
+                print("Buzz", end="")
+                continue
+            print("Buzz", end=" ")
+        elif (n % 3 == 0) and (n % 5 == 0):
+            print("FizzBuzz", end=" ")
+        else:
+            print(n, end=" ")
