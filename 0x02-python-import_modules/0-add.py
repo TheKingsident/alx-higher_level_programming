842f19afaa7825b908735a6f22b19c3212aa4fe4
Kingsley Usa <kingsident@gmail.com>
2023-08-11 11:53:15 +0100

    Added new assignment directory and files

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -6,7 +6,7 @@ from add_0 import add as addition
 def print_add():
     a = 1
     b = 2
-    print(f"{a} + {b} = {addition(a, b)}")
+    print("{}".format(a), "+ {}".format(b), "= {}".format(addition(a, b)))
 
 
 if __name__ == "__main__":
