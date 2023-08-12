46ca0825b15315bca732fcd54e216dff8125892d
Kingsley Usa <kingsident@gmail.com>
2023-08-12 03:12:52 +0100

    Added new assignment directory and files

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,9 +2,11 @@
 
 import hidden_4
 
+
 def main():
     strings = dir(hidden_4)
-    required_strings = sorted(string for string in strings if not string.startswith("__"))
+    required_strings = sorted(string for string in strings if
+                              not string.startswith("__"))
     for string in required_strings:
         print(string)
 
