954d2905943b029a05d1693c2430f004dcd5e619
Kingsley Usa <kingsident@gmail.com>
2023-08-12 12:09:02 +0100

    Added new assignment directory and files

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,32 +5,33 @@ from calculator_1 import add, sub, mul, div
 from sys import argv, exit
 
 
-arguments = argv[1:]
-number_of_args = len(arguments)
+def main():
+    arguments = argv[1:]
+    number_of_args = len(arguments)
 
-if number_of_args != 3:
-    print(f"Usage: ./100-my_calculator.py <a> <operator> <b>")
-    exit(1)
-else:
-    a = int(arguments[0])
-    b = int(arguments[2])
-    operator = arguments[1]
-    match operator:
-        case "+":
-            print(f"{a} {operator} {b} = {add(a, b)}")
-            exit(0)
-        case "-":
-            print(f"{a} {operator} {b} = {sub(a, b)}")
-            exit(0)
-        case "*":
-            print(f"{a} {operator} {b} = {mul(a, b)}")
-            exit(0)
-        case "/":
-            print(f"{a} {operator} {b} = {div(a, b)}")
-            exit(0)
-        case _:
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
+    if number_of_args != 3:
+        print(f"Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    else:
+        a = int(arguments[0])
+        b = int(arguments[2])
+        operator = arguments[1]
+        match operator:
+            case "+":
+                print(f"{a} {operator} {b} = {add(a, b)}")
+                exit(0)
+            case "-":
+                print(f"{a} {operator} {b} = {sub(a, b)}")
+                exit(0)
+            case "*":
+                print(f"{a} {operator} {b} = {mul(a, b)}")
+                exit(0)
+            case "/":
+                print(f"{a} {operator} {b} = {div(a, b)}")
+                exit(0)
+            case _:
+                print("Unknown operator. Available operators: +, -, * and /")
+                exit(1)
 
 
 if __name__ == "__main__":
