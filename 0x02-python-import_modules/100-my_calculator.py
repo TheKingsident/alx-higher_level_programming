b21937555fae53b7c2de7e082daf7022bf447c61
Kingsley Usa <kingsident@gmail.com>
2023-08-12 05:14:37 +0100

    Added new assignment directory and files

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -10,26 +10,26 @@ operator = arguments[1]
 number_of_args = len(arguments)
 
 if number_of_args != 3:
-    print(f"Usage: ./100-my_calculator.py <a> <operator> <b>\n1")
+    print(f"Usage: ./100-my_calculator.py <a> <operator> <b>")
     exit(1)
 else:
     a = int(arguments[0])
     b = int(arguments[2])
     match operator:
         case "+":
-            print(f"{a} {operator} {b} = {add(a, b)}\n0")
+            print(f"{a} {operator} {b} = {add(a, b)}")
             exit(0)
         case "-":
-            print(f"{a} {operator} {b} = {sub(a, b)}\n0")
+            print(f"{a} {operator} {b} = {sub(a, b)}")
             exit(0)
         case "*":
-            print(f"{a} {operator} {b} = {mul(a, b)}\n0")
+            print(f"{a} {operator} {b} = {mul(a, b)}")
             exit(0)
         case "/":
-            print(f"{a} {operator} {b} = {div(a, b)}\n0")
+            print(f"{a} {operator} {b} = {div(a, b)}")
             exit(0)
         case _:
-            print("Unknown operator. Available operators: +, -, * and /\n1")
+            print("Unknown operator. Available operators: +, -, * and /")
             exit(1)
 
 
