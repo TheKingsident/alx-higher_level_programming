79e4a34862fb625c752b4c786c95d69aa09a2ea5
Kingsley Usa <kingsident@gmail.com>
2023-08-13 17:49:03 +0100

    Added new assignment directory and files

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -8,6 +8,6 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for col_index, element in enumerate(row):
             if col_index == len(row) - 1:
-                print("{:d}".format(element))
+                print("{:d}".format(int(element)))
             else:
-                print("{:d}".format(element), end=" ")
+                print("{:d}".format(int(element)), end=" ")
