caf124e095b72cf92107a3b0feda11c149bfe934
Kingsley Usa <kingsident@gmail.com>
2023-08-14 14:38:50 +0100

    Added new assignment directory and files

--- 0x03-python-data_structures/8-main.py
+++ 0x03-python-data_structures/8-main.py
@@ -2,6 +2,7 @@
 multiple_returns = __import__('8-multiple_returns').multiple_returns
 
 sentence = "At school, I learnt C!"
-length, first = multiple_returns(sentence)
+sentence2 = ""
+length, first = multiple_returns(sentence2)
 print("Length: {:d} - First character: {}".format(length, first))
 
--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,7 +4,8 @@ def multiple_returns(sentence):
 
     if len(sentence) == 0:
         first_character = None
-    first_character = sentence[0]
+    else:
+        first_character = sentence[0]
 
     new_tuple = (len(sentence), first_character)
     return new_tuple
