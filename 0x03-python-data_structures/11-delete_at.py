c7364409793393afb6046d7460d74c035b912962
Kingsley Usa <kingsident@gmail.com>
2023-08-14 16:42:13 +0100

    Added new assignment directory and files

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 
 def delete_at(my_list=[], idx=0):
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return my_list
     else:
-        new_list = []
-        new_list = my_list[0:idx]
-        for num in my_list[3 + 1:]:
-            new_list.append(my_list[idx + 1:])
+        new_list = my_list[0:idx] + my_list[idx + 1:]
         return new_list
