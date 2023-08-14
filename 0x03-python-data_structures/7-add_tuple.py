0b0714d5685b615a7346c2ced971366dc3fd6e54
Kingsley Usa <kingsident@gmail.com>
2023-08-14 13:26:53 +0100

    Added new assignment directory and files

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    new_tuple = [0, 0]
+
+    if len(tuple_a) < 2:
+        tuple_a += (0,) * (2 - len(tuple_a))
+    if len(tuple_b) < 2:
+        tuple_b += (0,) * (2 - len(tuple_b))
+
+    for i in range(0, 2):
+        new_tuple[i] = tuple_a[i] + tuple_b[i]
+
+        return tuple(new_tuple)
