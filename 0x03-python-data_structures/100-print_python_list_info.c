1184c38b9d750aaf10b322c699f427f5c66a5f60
Kingsley Usa <77732519+TheKingsident@users.noreply.github.com>
2023-08-15 09:56:16 +0100

    Add files via upload

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,24 @@
+#include <Python.h>
+
+/**
+ * print_python_list_info - Prints basic information about Python lists
+ * @p: A pointer to a PyObject (Python list)
+ */
+void print_python_list_info(PyObject *p)
+{
+	int size;
+	int j;
+	PyObject *element
+
+	size = PyList_Size(p);
+
+	printf("[*] Size of the Python List = %d\n", size);
+	printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
+
+
+	for (j = 0; j < size; j++)
+	{
+		element = PyList_GetItem(p, j);
+		printf("Element %d: %s\n", i, Py_TYPE(element)->tp_name); }
+
+}
