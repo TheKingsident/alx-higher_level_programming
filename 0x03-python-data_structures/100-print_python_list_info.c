9897129ef0dba006f6899c656b7d02e713fbbe99
Kingsley Usa <kingsident@gmail.com>
2023-08-15 09:45:26 +0100

    Added new assignment directory and files

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -14,9 +14,8 @@ void print_python_list_info(PyObject *p)
 	printf("[*] Size of the Python List = %d\n", size);
 	printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
 
-	i = 0;
 
-	for (i; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		item = PyList_GetItem(p, i);
 		printf("Element %d: %s\n", i, Py_TYPE(item)->tp_name); }
