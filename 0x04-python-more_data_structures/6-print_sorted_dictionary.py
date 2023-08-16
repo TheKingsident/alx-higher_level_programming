474828d593536d1bfcbc073e624bf1cad51387fe
Kingsley Usa <kingsident@gmail.com>
2023-08-16 18:31:55 +0100

    Added new assignment directory and files

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,4 +3,4 @@
 def print_sorted_dictionary(a_dictionary):
     for key in sorted(a_dictionary.keys()):
         values = a_dictionary[key]
-        print(f"{key} : {values}")
+        print(f"{key}: {values}")
