4a79a0dbcfc5c857d509439e7fc163ac28ea40f4
Kingsley Usa <kingsident@gmail.com>
2023-08-16 16:06:27 +0100

    Added new assignment directory and files

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    squared_matrix = tuple(map(lambda row: [values ** 2 for values in row],
-                           matrix))
+    squared_matrix = list(map(lambda row: [values ** 2 for values in row],
+                          matrix))
     return squared_matrix
