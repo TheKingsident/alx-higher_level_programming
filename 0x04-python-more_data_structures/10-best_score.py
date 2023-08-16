f86bb658a1c4c57ccb96df4d15a2f5bc9ed4fefb
Kingsley Usa <kingsident@gmail.com>
2023-08-16 19:46:31 +0100

    Added new assignment directory and files

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,17 +1,9 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    largest_value = 0
-    largest_val_key = ""
 
     if a_dictionary == None:
         return None
-    else:
-        for key in a_dictionary.keys():
-            if a_dictionary[key] > largest_value:
-                largest_value = a_dictionary[key]
-                largest_val_key = key
-            else:
-                largest_value = largest_value
 
+    largest_val_key = max(a_dictionary, key=a_dictionary.get)
     return largest_val_key
