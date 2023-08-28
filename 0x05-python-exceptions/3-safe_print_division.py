de64b767836652d9bb0439ed872e197514921d2d
Kingsley Usa <kingsident@gmail.com>
2023-08-28 19:55:36 +0100

    Added assignment files

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -5,7 +5,7 @@ def safe_print_division(a, b):
 
     try:
         div_result = a/b
-    except (ZeroDivisionError, ValueError):
+    except (ZeroDivisionError, TypeError):
         return None
     finally:
         print("Inside result: {}".format(div_result))
