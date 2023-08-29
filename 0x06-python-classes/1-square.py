e2f378daaa780fcaa7caa4d072c74fde3ac46502
Kingsley Usa <kingsident@gmail.com>
2023-08-29 19:45:20 +0100

    Added assignment files

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+This module contains the class Square
+"""
+
+
+class Square:
+    """
+    Represents a square shape.
+
+    This class defines a square with a size private attribute.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a new instance of Square.
+        """
+        self.__size = size
