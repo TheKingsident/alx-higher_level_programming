808ce7c88aa449b001dcc40c862eee9dfffbcbe3
Kingsley Usa <kingsident@gmail.com>
2023-08-29 19:10:31 +0100

    Added assignment files

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 
 class Square:
+    """
+    Represents a square shape.
+
+    This class defines a square without any methods or attributes initially.
+    You can add methods and attributes to define its behavior and
+    characteristics.
+    """
     pass
