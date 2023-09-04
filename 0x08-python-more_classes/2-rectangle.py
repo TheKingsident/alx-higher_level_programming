19809a1d8db30b24c334c4b8aaa10d8c0ceac5ed
Kingsley Usa <kingsident@gmail.com>
2023-09-04 18:32:46 +0100

    Added new files

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -93,4 +93,6 @@ class Rectangle:
         Returns:
             int: The perimeter of the rectangle.
         """
+        if self.__width == 0 or self.__height == 0:
+            return 0
         return (2 * self.__height) + (2 * self.__width)
