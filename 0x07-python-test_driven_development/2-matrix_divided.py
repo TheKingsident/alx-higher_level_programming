4e6a50470f85494bee1ebf924e32540d44fbd00b
Kingsley Usa <kingsident@gmail.com>
2023-09-05 00:02:04 +0100

    Added new files

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This module deines a functoin that divides
+all elements of a matrix by a given number
+and returns a new matrix.
+"""
+
+
+def matrix_divided(matrix, div):
+    """
+    Divides all elements of a matrix by a given number.
+    """
+
+    if not isinstance(matrix, list) or not all(isinstance(row, list)
+                                               and all(isinstance(num, (int, float)) for num in row) for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+    row_sizes = set(len(row) for row in matrix)
+    if len(row_sizes) != 1:
+        raise TypeError("Each row of the matrix must have the same size")
+
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    new_matrix = [[round(num / div, 2) for num in row] for row in matrix]
+
+    return new_matrix
---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This module deines a function that prints names
+Prints the name in the format:
+My name is <first name> <last name>.
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Prints the name in the format: My name is <first name> <last name>.
+    """
+
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    if last_name:
+        print(f"My name is {first_name} {last_name}")
+    else:
+        print(f"My name is {first_name}")
