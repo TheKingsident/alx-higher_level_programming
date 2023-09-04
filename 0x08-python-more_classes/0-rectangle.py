f1fa904bb7fc4731b47f553b849f282c5e544573
Kingsley Usa <kingsident@gmail.com>
2023-09-04 16:07:45 +0100

    Added new files

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
 
+"""
+This module contains the rectangle class definintion
+"""
+
 class Rectangle:
+    """
+    Represents a rectangle shape.
+
+    This class defines a square without any methods or attributes initially.
+    You can add methods and attributes to define its behavior and
+    characteristics.
+    """
     pass
