2293927ac0d2848fcaeb9a92d80157105bea1ec6
Kingsley Usa <kingsident@gmail.com>
2023-09-05 10:39:41 +0100

    Added new files

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -5,26 +5,25 @@ This module is a program that solves the N queens problem.
 """
 import sys
 
+
 def is_safe(board, row, col):
     """
     Check if it's safe to place a queen at board[row][col].
     """
-    # Check the left side of the current row
     for i in range(col):
         if board[row][i] == 1:
             return False
-    
-    # Check upper diagonal on the left side
+
     for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
-    
-    # Check lower diagonal on the left side
+
     for i, j in zip(range(row, len(board), 1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
-    
+
     return True
+
 
 def solve_nqueens(n):
     """
@@ -33,14 +32,14 @@ def solve_nqueens(n):
     if n < 4:
         print("N must be at least 4")
         sys.exit(1)
-    
+
     board = [[0 for _ in range(n)] for _ in range(n)]
     solutions = []
 
     def backtrack(col):
         if col >= n:
-            # All queens are placed successfully, add this solution to the list
-            solutions.append([(i, row.index(1)) for i, row in enumerate(board)][::-1])
+            solutions.append([(i, row.index(1))
+                              for i, row in enumerate(board)][::-1])
             return
 
         for i in range(n):
@@ -50,24 +49,26 @@ def solve_nqueens(n):
                 board[i][col] = 0
 
     backtrack(0)
-    
+
     return solutions
+
 
 def main():
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-    
+
     try:
         n = int(sys.argv[1])
     except ValueError:
         print("N must be a number")
         sys.exit(1)
-    
+
     solutions = solve_nqueens(n)
-    
+
     for solution in solutions:
         print([list(row) for row in solution])
+
 
 if __name__ == "__main__":
     main()
