e2ab92dbdc87de21364040119afe2152124950fd
Kingsley Usa <kingsident@gmail.com>
2023-09-11 22:18:09 +0100

    Added new files

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
-Rectangle = __import__('9-rectangle').Rectangle
 """
 A module that defines a class representing a square, inheriting from
 Rectangle.
 """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
