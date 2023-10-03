3944b70462dd172e504ee3ebf09b28d74d658f49
Kingsley Usa <kingsident@gmail.com>
2023-10-03 17:22:42 +0100

    Added new files

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+prints a text with 2 new lines after each of these
+characters: ., ? and :
+Args:
+    text (str): The text
+"""
+
+
+def text_indentation(text):
+    """
+    prints a text with 2 new lines after each of these
+    characters: ., ? and :
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    line = ""
+    for char in text:
+        line += char
+        if char in '.?:':
+            print(line.strip(), end="\n\n")
+            line = ""
+            
+    if line:
+        print(line.strip())
