d428d6c218e8df18e5e03b20f6ddbfd06806a996
cakemurderer <kingsident@gmail.com>
2023-11-14 22:10:30 +0100

    Added new files

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,4 @@
+-- Computes the score average of all records
+-- Table is second_table
+
+SELECT AVG(score) AS average FROM second_table;
---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,4 @@
+-- lists all records of the table
+-- Table is second_table
+
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
