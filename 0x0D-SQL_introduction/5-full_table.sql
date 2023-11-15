1d3c861d75edc539d3f5b13945ce8ecc3831e079
cakemurderer <kingsident@gmail.com>
2023-11-15 13:01:51 +0100

    Added new files

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,4 @@
 -- Prints full description the table in the current database
 -- Table is called first_table
 
-SHOW CREATE TABLE hbtn_0c_0.first_table;
+SHOW CREATE TABLE hbtn_test_db_5.first_table;
