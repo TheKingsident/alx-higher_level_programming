25200adec624b1b3a42e25b1b342a6494ff80223
cakemurderer <kingsident@gmail.com>
2023-12-16 13:31:13 +0100

    Added new files

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (Number.isInteger(w) && Number.isInteger(h) && w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      return {};
+    }
+  }
+}
+
+module.exports = Rectangle;
