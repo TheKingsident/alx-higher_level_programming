97c7e35f0ff8856b9e5e81c37a54ee685317c303
cakemurderer <kingsident@gmail.com>
2023-12-16 21:16:29 +0100

    Added new files

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const [,, sourceFile1, sourceFile2, destFile] = process.argv;
+
+fs.readFile(sourceFile1, 'utf8', (err, data1) => {
+  if (err) throw err;
+
+  fs.readFile(sourceFile2, 'utf8', (err, data2) => {
+    if (err) throw err;
+
+    const combinedData = data1 + '\n' + data2;
+
+    fs.writeFile(destFile, combinedData, 'utf8', (err) => {
+      if (err) throw err;
+    });
+  });
+});
