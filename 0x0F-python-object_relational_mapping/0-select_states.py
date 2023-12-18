67ce31f2873a8cb09fd0cd7900f87cd7f02904c1
cakemurderer <kingsident@gmail.com>
2023-12-18 01:57:24 +0100

    Added new file

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import MySQLdb
+import sys
+
+"""
+List states
+"""
+
+
+def list_states(mysql_username, mysql_password, db_name):
+    """
+    List all states from the database.
+    """
+    db = MySQLdb.connect(host="localhost", port=3306, user=mysql_username,
+                         passwd=mysql_password, db=db_name)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 4:
+        mysql_username = sys.argv[1]
+        mysql_password = sys.argv[2]
+        db_name = sys.argv[3]
+        list_states(mysql_username, mysql_password, db_name)
