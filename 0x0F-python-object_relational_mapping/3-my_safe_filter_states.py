01f5f20712e8e2fd47dd3883a8a17065d2584ef3
cakemurderer <kingsident@gmail.com>
2023-12-21 20:33:49 +0100

    Added new files

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Module script that list states """
+import MySQLdb
+import sys
+
+
+def search_states_safe():
+    """ List all states from the database. """
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+
+    nameSch = sys.argv[4]
+    query = "SELECT * FROM {} WHERE name = %s ORDER BY id ASC".format('states')
+    cur = db.cursor()
+    cur.execute(query, (nameSch,))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    search_states_safe()
