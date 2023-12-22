fd6f175b4fee1ef3865a15dff526b1ef15c008cb
cakemurderer <kingsident@gmail.com>
2023-12-22 23:14:06 +0100

    Added 101-relationship_states_cities_list.py

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Lists all State objects, and corresponding City objects
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, joinedload
+from relationship_state import State
+import sys
+
+
+def list_states_and_cities(mysql_username, mysql_pswd, db_name):
+    """
+    Lists all State objects and their corresponding City objects.
+    """
+    engine = create_engine(
+        f'mysql+mysqldb://{mysql_username}:{mysql_pswd}@localhost/{db_name}'
+    )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Querying all states with their related cities
+    from relationship_city import City
+    states = session.query(State).options(joinedload(State.cities)).order_by(
+        State.id, City.id).all()
+
+    for state in states:
+        print(f"{state.id}: {state.name}")
+        for city in state.cities:
+            print(f"\t{city.id}: {city.name}")
+
+    session.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 4:
+        mysql_username = sys.argv[1]
+        mysql_pswd = sys.argv[2]
+        db_name = sys.argv[3]
+        list_states_and_cities(mysql_username, mysql_pswd, db_name)
