ebd7183e052f7cd4e493b2511029392feb74df82
cakemurderer <kingsident@gmail.com>
2024-01-05 21:58:06 +0100

    Modified 3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL and displays all HTTP methods the server will accept.
-curl -X OPTIONS -i -s 0.0.0.0:5000/route_4 | grep 'Allow:' | cut -d ' ' -f2- | tr -d '\r'
+curl -X OPTIONS -i -s "http://$1" | grep 'Allow:' | cut -d ' ' -f2- | tr -d '\r'
