1f9a4422cd37d2d1979809bb76851d39e42eaeca
cakemurderer <kingsident@gmail.com>
2024-01-07 20:48:07 +0100

    Added 7-error_code.py

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Sends a request to the URL and displays the body of the response."""
+
+import sys
+import requests
+from requests.exceptions import HTTPError
+
+
+def error_code(url):
+    """Sends a request to the URL and displays the body of the response."""
+    response = requests.get(url)
+    if response.status_code >= 400:
+        print("Error code:", response.status_code)
+    else:
+        print(response.text)
+    
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    error_code(url)
