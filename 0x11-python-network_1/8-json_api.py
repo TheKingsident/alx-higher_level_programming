5bc6c9e01b0e878ec90f2ea05ae4273e657d3503
cakemurderer <kingsident@gmail.com>
2024-01-07 21:27:02 +0100

    Updated 8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -11,14 +11,14 @@ def main():
         q = sys.argv[1]
     else:
         q = ""
-        
+
     response = requests.post('http://0.0.0.0:5000/search_user', {'q': q})
 
     try:
         json_response = response.json()
         if json_response:
             print("[{}] {}".format(json_response.get('id'),
-                                json_response.get('name')))
+                                   json_response.get('name')))
         else:
             print("No result")
     except ValueError:
