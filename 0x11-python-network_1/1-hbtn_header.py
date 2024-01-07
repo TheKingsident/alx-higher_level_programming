f87b6dad94a215a5ac8f9e767b07feca28f38d65
cakemurderer <kingsident@gmail.com>
2024-01-07 15:21:01 +0100

    Added 1-hbtn_header.py

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""sends a request to the URL and displays the value of the X-Request-Id"""
+
+import sys, urllib.request
+
+
+def fetch_variable(url):
+    """Definition of the module"""
+    with urllib.request.urlopen(url) as response:
+        x_request = response.getheader('X-Request-Id')
+        print(x_request)
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    fetch_variable(url)
