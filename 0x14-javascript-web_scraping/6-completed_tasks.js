af09e29209f8e8a8f9f0cceda87be584ca3bc10f
cakemurderer <kingsident@gmail.com>
2024-01-31 21:45:40 +0100

    Added new file

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,35 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = process.argv[2];
+
+request(url, (error, response, body) => {
+    if (error) {
+        console.error('Error fetching data:', error);
+        return;
+    }
+
+    try {
+        const tasks = JSON.parse(body);
+        const completedTasksByUser = countCompletedTasksByUser(tasks);
+        console.log(completedTasksByUser);
+    } catch (parseError) {
+        console.error('Error parsing JSON:', parseError);
+    }
+});
+
+function countCompletedTasksByUser(tasks) {
+    const completedTasksCount = {};
+
+    tasks.forEach(task => {
+        if (task.completed) {
+            if (!completedTasksCount[task.userId]) {
+                completedTasksCount[task.userId] = 0;
+            }
+            completedTasksCount[task.userId]++;
+        }
+    });
+
+    return completedTasksCount;
+}
