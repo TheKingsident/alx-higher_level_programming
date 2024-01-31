5aa8299cb31f043969c92eaf8632df73e0c4591c
cakemurderer <kingsident@gmail.com>
2024-01-31 21:48:08 +0100

    Updated

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -5,31 +5,31 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, (error, response, body) => {
-    if (error) {
-        console.error('Error fetching data:', error);
-        return;
-    }
+  if (error) {
+    console.error('Error fetching data:', error);
+    return;
+  }
 
-    try {
-        const tasks = JSON.parse(body);
-        const completedTasksByUser = countCompletedTasksByUser(tasks);
-        console.log(completedTasksByUser);
-    } catch (parseError) {
-        console.error('Error parsing JSON:', parseError);
-    }
+  try {
+    const tasks = JSON.parse(body);
+    const completedTasksByUser = countCompletedTasksByUser(tasks);
+    console.log(completedTasksByUser);
+  } catch (parseError) {
+    console.error('Error parsing JSON:', parseError);
+  }
 });
 
-function countCompletedTasksByUser(tasks) {
-    const completedTasksCount = {};
+function countCompletedTasksByUser (tasks) {
+  const completedTasksCount = {};
 
-    tasks.forEach(task => {
-        if (task.completed) {
-            if (!completedTasksCount[task.userId]) {
-                completedTasksCount[task.userId] = 0;
-            }
-            completedTasksCount[task.userId]++;
-        }
-    });
+  tasks.forEach(task => {
+    if (task.completed) {
+      if (!completedTasksCount[task.userId]) {
+        completedTasksCount[task.userId] = 0;
+      }
+      completedTasksCount[task.userId]++;
+    }
+  });
 
-    return completedTasksCount;
+  return completedTasksCount;
 }
