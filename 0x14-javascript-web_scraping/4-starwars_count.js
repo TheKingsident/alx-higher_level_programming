bf8b5953795d80ee75f9e5b903110792551b016e
cakemurderer <kingsident@gmail.com>
2024-01-31 20:51:07 +0100

    Updated

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -6,45 +6,43 @@ const baseURL = process.argv[2];
 const characterID = 18;
 let characterCount = 0;
 
-function checkCharacterInMovie(movieId) {
-    return new Promise((resolve, reject) => {
-        const fullUrl = `${baseURL}/${movieId}`;
+function checkCharacterInMovie (movieId) {
+  return new Promise((resolve, reject) => {
+    const fullUrl = `${baseURL}/${movieId}`;
 
-        request(fullUrl, { json: true }, (err, res, body) => {
-            if (err) {
-                reject(err);
-                return;
-            }
+    request(fullUrl, { json: true }, (err, res, body) => {
+      if (err) {
+        reject(err);
+        return;
+      }
 
-            if (!body || !body.characters) {
-                console.error(`No 'characters' field in response for movie ID ${movieId}`);
-                resolve(false);
-                return;
-            }
+      if (!body || !body.characters) {
+        resolve(false);
+        return;
+      }
 
-            const characterUrl = `https://swapi-api.alx-tools.com/api/people/${characterID}/`;
-            if (body.characters.includes(characterUrl)) {
-                resolve(true);
-            } else {
-                resolve(false);
-            }
-        });
+      const characterUrl = `https://swapi-api.alx-tools.com/api/people/${characterID}/`;
+      if (body.characters.includes(characterUrl)) {
+        resolve(true);
+      } else {
+        resolve(false);
+      }
     });
+  });
 }
 
-async function main() {
-    try {
-        for (let movieId = 1; movieId <= 7; movieId++) {
-            const hasCharacter = await checkCharacterInMovie(movieId);
-            if (hasCharacter) {
-                characterCount++;
-            }
-        }
-
-        console.log(`${characterCount}`);
-    } catch (error) {
-        console.error('An error occurred:', error);
+async function main () {
+  try {
+    for (let movieId = 1; movieId <= 7; movieId++) {
+      const hasCharacter = await checkCharacterInMovie(movieId);
+      if (hasCharacter) {
+        characterCount++;
+      }
     }
+
+    console.log(`${characterCount}`);
+  } catch (error) {
+  }
 }
 
 main();
