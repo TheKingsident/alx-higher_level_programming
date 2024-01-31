36ed671a98b0051b947c94b159e14ff4626fd60b
cakemurderer <kingsident@gmail.com>
2024-01-31 13:31:45 +0100

    Updated

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -6,6 +6,7 @@ const url = process.argv[2];
 request(url, (error, response) => {
   if (error) {
     console.error(error);
+    return;
   }
-  console.log('code: ', response && response.statusCode);
+  console.log(`code: ${response && response.statusCode}`);
 });
